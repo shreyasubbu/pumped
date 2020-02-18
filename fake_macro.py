a7335740b9409d70e6f782e8e760a2218fa59f94
unknown <shreya.subbu@gmail.com>
2020-02-18 13:58:07 -0500

    updates to fake data

--- fake_macro.py
+++ fake_macro.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import numpy as np
+import datetime
 
 pd.set_option('display.max_columns', 10)
+pd.set_option('display.max_rows', 50)
 
 pump_power_corr = 1
 session_length_corr = 1
@@ -9,30 +11,48 @@ interval_corr = 1
 time_corr = 1
 frequency_corr = 1
 
+#Randomly choosing ideal session length
 ideal_sessions = np.random.random_integers(3, 7)
 
-def time_of_day(time):
-    if time.hour < 6:
-        time_in_day = "early_morning"
-    elif time.hour >=6 and time.hour < 12:
-        time_in_day = "morning"
-    elif time.hour >=12 and time.hour < 18:
-        time_in_day = "afternoon"
-    else:
-        time_in_day = "night"
+#Identifying time of day
+time_in_day = ['early_morning', "morning", "afternoon", "night"]
 
+def time_of_day(hour):
+    if hour < 6:
+        time_index = 0
+    elif hour >=6 and hour < 12:
+        time_index = 1
+    elif hour >=12 and hour < 18:
+        time_index = 2
+    else:
+        time_index = 3
+    return time_index
+
+#best time of day
+best_time = np.random.random_integers(0,3)
 
 # creating dataframe with daterange
+start_date = datetime.datetime(2020, 1, 1, 6)
 date_rng = pd.date_range(start='1/1/2020 06:00:00', periods=8, freq='2H')
 df = pd.DataFrame(date_rng, columns=['date'])
-df['date'] = pd.to_datetime(df['date'])
 
+for i in range(2,16):
+    start_date = start_date + datetime.timedelta(days=1)
+    new_dates = pd.date_range(start=start_date, periods=8, freq='2H')
+    new_df = pd.DataFrame(new_dates, columns=['date'])
+    df_array = [df, new_df]
+    df = pd.concat(df_array, ignore_index=True)
+
+print(df)
+
+df['date'] = pd.to_datetime(df['date'])
 
 # filling out other data frame values
 for sessions in range(len(df.index)):
     df.loc[sessions,'session_length'] = np.random.uniform(15, 40)
     df.loc[sessions,'pump_power'] = np.random.random_integers(1, 12)
     df.loc[sessions,'let_down_time'] = np.random.uniform(2, 10)
+    df.loc[sessions, 'time_of_day'] = time_of_day(df.loc[sessions, 'date'].hour)
 
     # First pump session should have interval of 0
     if sessions is 0:
@@ -46,9 +66,9 @@ for sessions in range(len(df.index)):
     else:
         df.loc[sessions, 'num_sessions'] = df.loc[(sessions - 1), 'num_sessions'] + 1
 
-    #Creating milk volume measurement as result of other factors
-    #milk volume based on session length
-    df.loc[sessions, 'milk_vol'] = df.loc[sessions,'session_length'] * session_length_corr + np.random.normal(0, 4)
+    #Creating milk volume measurement as result of several factors
+    #milk volume is initially based on session length
+    df.loc[sessions, 'milk_vol'] = df.loc[sessions,'session_length'] + (np.random.normal(0, 4) * session_length_corr)
 
     #added volume based on pump power
     power_vol = (df.loc[sessions,'pump_power'] * pump_power_corr) / 100
@@ -65,7 +85,11 @@ for sessions in range(len(df.index)):
         freq_vol_noise = freq_vol + np.random.normal(0, freq_vol)
         df.loc[sessions, 'milk_vol'] = df.loc[sessions, 'milk_vol'] + freq_vol_noise
 
-
+    #Add volume based on best time of day
+    if df.loc[sessions, 'time_of_day'] is best_time:
+        time_vol_noise = (time_corr * 0.5) + (np.random.normal(0, 0.1) * time_corr)
+        df.loc[sessions, 'milk_vol'] = df.loc[sessions, 'milk_vol'] + time_vol_noise
 
 print(df)
+#df.to_excel(excel_writer='sample_data.xlsx', sheet_name='sheet1',index_label=False)
 
