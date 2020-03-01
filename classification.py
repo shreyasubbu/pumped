026a44af517692ce070f1c2c574cd4b02ef1da02
unknown <shreya.subbu@gmail.com>
2020-03-01 03:11:14 -0500

    Started classification

---
+++ classification.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn import preprocessing
+import pandas as pd
+import matplotlib.pyplot as plt
+import pylab as pl
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn import metrics
+
+
+df = pd.read_excel('sample_data.xlsx', header=0, index_col=0)
+df = df[['session_length', 'pump_power', 'time_of_day', 'num_sessions', 'milk_vol']]
+df['milk_rate'] = df['milk_vol'] / df['session_length']
+df = df.drop(['milk_vol', 'session_length'], axis=1)
+
+min_vol = min(df['milk_rate'])
+max_vol = max(df['milk_rate'])
+
+classes = np.linspace(min_vol, max_vol, 4)
+print(classes)
+class_ = [1, 2, 3]
+
+for session in range(len(df.index)):
+    if df.loc[session, 'milk_rate'] <= classes[1]:
+        df['label'] = class_[0]
+        print('0')
+    elif df.loc[session, 'milk_rate'] > classes[1] and df.loc[session, 'milk_rate'] <= classes[2]:
+        df['label'] = class_[1]
+        print('1')
+    else:
+        df['label'] = class_[2]
+        print('2')
+
+X = np.array(df.drop(['milk_rate', 'label'], axis = 1))
+X = preprocessing.scale(X)
+y = np.array(df['label'])
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+clf = KNeighborsClassifier(n_neighbors=5)
+clf.fit(X_train, y_train)
+
+y_pred = clf.predict(X_test)
+
+print(metrics.accuracy_score(y_test, y_pred))
--- clustering.py
+++ clustering.py
@@ -1,19 +1,35 @@
 import numpy as np
 from sklearn.cluster import KMeans
+from sklearn.decomposition import PCA
 from sklearn import preprocessing
 import pandas as pd
 import matplotlib.pyplot as plt
+import pylab as pl
 
+
+# ok so clustering with k-means didn't work out too great lol but thats ok we can come back to it??
 df = pd.read_excel('sample_data.xlsx', header=0, index_col=0)
 df = df[['session_length', 'pump_power', 'time_of_day', 'num_sessions', 'milk_vol']]
+df['milk_rate'] = df['milk_vol'] / df['session_length']
+df = df.drop(['milk_vol', 'session_length'], axis=1)
 
 X = np.array(df)
 X = preprocessing.scale(X)
 
+pca = PCA(n_components=2).fit(X)
+pca_2d = pca.transform(X)
+
+
 clf = KMeans(n_clusters=3)
 clf.fit(X)
 
-print(clf.labels_)
 
-plt.scatter(clf.labels_, np.array(df['milk_vol']))
-plt.show()
+for var in df:
+    pl.figure(var)
+    pl.scatter(df[var], df['milk_rate'], c=clf.labels_)
+    pl.show()
+
+#print(clf.labels_)
+
+#plt.scatter(clf.labels_, np.array(df['milk_vol']))
+#plt.show()
